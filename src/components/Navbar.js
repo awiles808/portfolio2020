19321c90c72b3e138187f2a197eec600f770de82
awiles808 <awiles808@gmail.com>
2020-07-28 17:08:18 -0600

    Avatar now displaying and listItems

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
 import {
     AppBar,
     Toolbar,
     ListItem,
+    ListItemIcon,
     IconButton,
     ListItemText,
     Avatar,
     Divider,
     List,
-    Typograpgy,
-    Box
+    Typography,
+    Box,
 } from "@material-ui/core";
 import {
     ArrowBack,
@@ -17,21 +20,81 @@ import {
     Home,
     Apps,
     ContactMail
-}from "@material-ui/icons";
+} from "@material-ui/icons";
+import avatar from "../logo.png";
+
+const useStyles = makeStyles(theme => ({
+    menuSliderContainer: {
+        width: 250,
+        background: '#96CDD4',
+        height: "30rem"
+    },
+    avatar: {
+        display:"block",
+        margin: "0.5rem auto",
+        width: theme.spacing(15),
+        height: theme.spacing(15)
+    },
+    listItem: {
+        color: '#222'
+    }
+    
+}));
+
+const menuItems = [
+    {
+        listIcon: <Home />,
+        listText: "Home"
+    },
+    {
+        listIcon: <AssignmentInd />,
+        listText: "Resume"
+    },
+    {
+        listIcon: <Apps/>,
+        listText: "Portfolio"
+    },
+    {
+        listIcon: <ContactMail />,
+        listText: "Contacts"
+    }
+]
 
 const Navbar = () => {
+    const classes = useStyles()
     return (
-        <Box component="nav">
-        <AppBar postition="static" style={{ background: "#222"}}>
-            <Toolbar>
-                    <ArrowBack style={{ color: "#96CDD4"} }/>
-            </Toolbar>
+        <>
+            <Box className={classes.menuSliderContainer} component="div">
+                <Avatar className={classes.avatar} src={avatar} alt="Auburn" />
+                <Divider />
+                <List>
+                    {menuItems.map((lsItem, key) => ( 
+                    <ListItem button key={key}>
+                        <ListItemIcon className={classes.listItem}>
+                            {lsItem.listIcon}
+                        </ListItemIcon>
+                        <ListItemText 
+                            clasName={classes.listItem}
+                            primary={lsItem.listText}/>
+                    </ListItem>
+                    ))}
+                </List>
+            </Box>
+            <Box component="nav">
+                <AppBar position="static" style={{ background: "#222" }}>
+                    <Toolbar>
+                        <IconButton>
+                            <ArrowBack style={{ color: "#96CDD4" }} />
+                        </IconButton>
+                        <Typography variant="h5" style={{ color: "#FFFFFF" }}>
+                            Portfolio
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </>
 
-        </AppBar>
-
-            
-        </Box>
-    )
-}
+    );
+};
 
 export default Navbar
