e3dbb8b8566e28242e085b2f8295b9554b271628
awiles808 <awiles808@gmail.com>
2020-08-01 12:25:54 -0600

    Added Resume Component And Styled With Timeline And Year

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import {Route} from 'react-router-dom'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
 import Home from './components';
-import Resume from "./components/Resume"
+import Resume from "./components/Resume";
+// import Portfolio from "./components/Portfolio";
+// import Contacts from "./components/Contacts"
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
  <CssBaseline/>
   <Route exact path="/" component={Home}/>
   <Route path="/resume" component={Resume}/>
+  {/* <Route path="/portfolio" component={Portfolio}/>
+  <Route path="/contacts" component={Contacts}/> */}
 
 </>
 
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,16 +3,114 @@ import {makeStyles} from "@material-ui/core/styles"
 import {Typography, Box} from "@material-ui/core";
 import Navbar from "./Navbar";
 
+const useStyles = makeStyles (theme=>({
+    mainContainer: {
+        background: "#96CDD4"
+    },
+    timeLine: {
+        position: "relative",
+        padding: "1rem",
+        margin: "0 auto",
+        "&:before": {
+            content:"''",
+            position: "absolute",
+            height: "100%",
+            border: "1px solid black",
+            right: "40px",
+            top: 0
+        },
+        "&:after": {
+            content: "''",
+            display: "table",
+            clear: "both"
+        },
+        [theme.breakpoints.up("md")]:{
+            padding: "2rem",
+            "&:before": {
+                left: "calc(50% - 1px)",
+                right: "auto"
+            }
+        }
+    },
+    timeLineItem: {
+        padding: "1rem",
+        borderBottom: "2px solid black",
+        position: "relative",
+        margin: "1rem 3rem 1rem 1rem",   
+        clear: "both",
+        "&:after": {
+            content: "''",
+            position: "absolute"
+        },
+        "&:before": {
+            content: "''",
+            position: "absolute",
+            right: "-0.625rem",
+            top: "calc(50% - 5px)",
+            borderStyle: "solid",
+            borderColor: "tomato tomato transparant transparant",
+            borderWidth: "0.625rem",
+            transform: "rotate(45deg)"
+            
+        },
+        [theme.breakpoints.up("md")]: {
+            width: "44%",
+            margin: "1rem",
+            "&:nth-of-type(2n)": {
+                float: "right",
+                margin: "1rem",
+                borderColor: "tan"
+            },
+            "&:nth-of-type(2n):before": {
+                right: "auto",
+                left: "-0.625rem",
+                borderColor: "transparant transparant tomato tomato"
+            }
+        }
+
+    },
+    timeLineYear: {
+        textAlign: "center",
+        maxWidth: "9.375rem",
+        margin: "0 3rem 0 auto", 
+        fontSize: "1.8rem",
+        background: "tomato",
+        color: "white",
+        lineHeight: 1,
+        padding: "0.5rem 0 1rem",
+        "&:before" : {
+            display: "none"
+        },
+        [theme.breakpoints.up("md")]: {
+            textAlign: "center",
+            margin: "0 auto",
+            "&:nth-of-type(2n)" : {
+                float: "none", 
+                margin: "0 auto"
+            },
+            "&:nth-of-type(2n):before": {
+                display: "none"
+            }
+        }
+    }
+}));
+
 const Resume = () => {
+    const classes = useStyles();
     return (
         <>
        <Navbar />
-      <Box component="header">
+      <Box component="header" className={classes.mainContainer}>
           <Typography variant="h4" align="center">
             Work Experience
           </Typography>
-          <Box component="div">
-              <Typography variant="h2">2020</Typography>
+          <Box component="div" className={classes.timeLine}>
+              <Typography 
+              variant="h2" 
+              className={`${classes.timeLineYear} ${classes.timeLineItem}`}
+              >
+                2020
+            </Typography>
           </Box>
       </Box>
        </>
